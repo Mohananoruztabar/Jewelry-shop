38a74daec4e0db19ede54c81cf7f5b0e0224ce8c
mohana-nr <@gmail.com>
2025-10-31 13:45:32 -0700

    add Shopping Cart functionality

---
+++ src/component/CartItem.jsx
@@ -0,0 +1,54 @@
+"use client"
+import { ShoppingCartItem } from "@/context/ShoppingCartItem"
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import ButtonItem from './ButtonItem'
+import Cart from "@/app/cart/page"
+
+function CartItem({id}) {
+
+    const { carts } = useContext(ShoppingCartItem)
+
+    const [items , setItems] = useState({})
+
+
+    
+    useEffect(()=>{
+        if (!id) return;
+
+        fetch(`http://localhost:8000/products/${id}`)
+        .then((res) => res.json())
+        .then((data) => setItems(data))
+
+        
+    } , [id])
+
+    if (!items?.id) return null
+
+  const cartItem = carts.find(item => item.id === id)
+  const quantity = cartItem?.qty || 0
+  const subTotal = ((items.price || 0) * quantity).toFixed(2)
+
+    
+  return (
+    <div>
+        <div className='grid grid-cols-12 border border-trans mb-2'>
+            
+            <img className='grid col-span-3 object-cover' src={items.imd} alt="" />
+            <div className='grid col-span-9'>
+                
+                <div className='grid grid-cols-8 items-center '>
+                    <h3 className='grid col-span-3'>{items.name}</h3>
+                    <h4 className='grid col-span-1'>{items.price}</h4>
+                    <h4 className='grid col-span-1'>{subTotal}$</h4>
+                    <div className='grid col-span-3 mb-3 me-2'>
+                        <ButtonItem id={id}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartItem
