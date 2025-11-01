a7aff2a39ef2efeba96cc280f6a9cfa836c7d846
mohana-nr <@gmail.com>
2025-11-01 12:52:45 -0700

    Created Blog section to display articles, news, and updates.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import Container from '@/component/Container'
 import BlogsItem from '@/component/BlogsItem'
 import Link from 'next/link'
+import dataapi from "@/dataBase/db.json"
 
 async function Blog() {
 
-  const result = await fetch("http://localhost:8000/blog")
-  const data = await result.json()
-
+  const blogs = dataapi.blog;
   
   return (
     <div>
       <Container>
         <h2 className='mt-10 text-4xl'>BLOG</h2>
         <div className='grid grid-cols-3 gap-6 mt-10'>
-          {data.map((item , i) => {
+          {blogs.map((item , i) => {
             return (
               <Link key={item.id} href={`/blog/${item.id}`}><BlogsItem  {...item} /></Link>
             )
