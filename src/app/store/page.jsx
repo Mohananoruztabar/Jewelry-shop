7e2d4406f29c236b51fc86a6104e5f546b1a408b
mohana-nr <@gmail.com>
2025-11-01 03:29:55 -0700

    Created Store page to showcase all products and allow browsing by categories.

---
+++ src/app/store/page.jsx
@@ -0,0 +1,52 @@
+import Container from '@/component/Container'
+import Paginate from '@/component/Paginate'
+import ProductItem from '@/component/ProductItem'
+import SliderShop from '@/component/SliderShop'
+import Link from 'next/link'
+import React from 'react'
+
+
+async function getProduct(page  , per_page , name){
+  const result = await fetch(`http://localhost:8000/products?_page=${page}&_per_page=${per_page}&name=${name}`,
+    { cache: "no-store" }
+  )
+  const data = await result.json()
+  return data
+}
+
+async function Store(props) {
+
+
+  const page =  (await props.searchParams)?.page || "1"
+  const per_page =  (await props.searchParams)?.per_page || "12"
+  const name = (await props.searchParams)?.name || ""
+
+
+  const products = await getProduct(page , per_page , name )
+  
+
+  
+  return (
+    <div>
+        <Container>
+            <h2 className='font-bold text-4xl text-center mt-12'>Shop</h2>
+            <div>
+                <SliderShop />
+            </div>
+            <div className='grid grid-cols-4'>
+              {products.data.map((item , i) =>{
+                return(
+                  <Link key={i} href={`/store/product/${item.id}`}>
+                    <ProductItem key={item.id} {...item}  />
+                  </Link>
+                  )
+              })}
+            </div>
+
+            <Paginate pageCount={products.pages}/>
+          </Container>
+    </div>
+  )
+}
+
+export default Store
